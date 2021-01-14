ecf1453af86862360561d74ebd13bf52fe240ee9
Klordok <blargsmarg@gmail.com>
2021-01-13 17:57:06 -0700

    outline components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import SearchForm from './SearchForm';
+import DisplayResults from './DisplayResults';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <h1>GOOGLE BOOK SEARCH</h1>
+      <SearchForm />
+      <DisplayResults />
     </main>
   );
 }
